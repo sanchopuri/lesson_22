5a1e17f91a2232bdc5c923f6ff4a54b184fda60d
sanchopuri <112719289+sanchopuri@users.noreply.github.com>
2022-12-02 21:36:31 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+
+
+console.log('я учу JS');
+
+//вариант 1 - верно
+
+//вариант 2 - верно
+
+//вариант 3 - не верно
+//надо поставить ; между значениями
+
+//вариант 4 - не верно
+//в первом значении не закрыты кавычки 
+
+
+
+
+
+
+
+
+
+let colorEyes = 'green'
+
+
+let userName = 'Вася';
+let user = 'Вася';
+userName = user;
+console.log(user);
+
+//вариант 1 - не верно
+//не хватает запятой между значениями
+
+//вариант 2 - верно
+
+//вариант 3 - верно
+
+//вариант 4 -  верно
+
+//вариант 5 -  верно
+
+
+
+
+
+
+
+
+let userNumber;
+console.log(typeof userNumber);
+console.log(userNumber);
+
+let block = document.querySelector('.block');
+console.log(block);
+
+
+let YouBad = false;
+if (YouBad) {
+	console.log('yea');
+} else {
+	console.log('noo');
+}
+
+
+
+//вариант 1 - не верно
+//нужны другие ковычки
+
+//вариант 2 - не верно
+//вернеться значение Infinity
+
+//вариант 3 - не верно
+//вернеться значение Undefined
+
+//вариант 4 - верно
+
